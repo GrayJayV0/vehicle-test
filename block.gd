4f18e309b96b4a005e436faa2505c32a7aa954cf
GrayJayV0 <student.150087@worcesterschools.net>
2024-09-20 12:55:17 -0400

    b

--- block.gd
+++ block.gd
@@ -11,4 +11,4 @@ func _process(delta: float) -> void:
 	if grounded.is_colliding():
 		#This vector is broken its missing a value needed to allow jumping
 		#Vectors are writen in (x,y,z) what value needs to be changed to allow jumping
-		apply_impulse(Vector3(0,1,0))
+		apply_impulse(Vector3(0,0,0))
--- car.gd
+++ car.gd
@@ -4,24 +4,35 @@ extends VehicleBody3D
 @onready var flwheel = $VehicleWheel3D2
 @onready var frwheel = $VehicleWheel3D3
 @export var MAX_STEER = 1
-@export var ENGINE_POWER = 800
+@export var ENGINE_POWER = 2000
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	steering = move_toward(steering, Input.get_axis("ui_right","ui_left") * MAX_STEER, delta * 10)
-	engine_force = Input.get_axis("ui_down","ui_up") * ENGINE_POWER
-	if Input.is_action_pressed("drift") :
-		blwheel.wheel_friction_slip = 0
-		brwheel.wheel_friction_slip = 0
-		
+	var drift
+	if Input.is_action_pressed("drift") and Input.get_axis("ui_right","ui_left") != 0:
+		drift = true
+		steering = Input.get_axis("ui_right","ui_left")
+		engine_force = 0
 	if Input.is_action_just_released("drift"):
 		blwheel.wheel_friction_slip = 10.5
 		brwheel.wheel_friction_slip = 10.5
+		flwheel.wheel_friction_slip = 10.5
+		frwheel.wheel_friction_slip = 10.5
+		drift = false
+		
+		
+	if drift != true:
+		steering = move_toward(steering, Input.get_axis("ui_right","ui_left") * MAX_STEER, delta * 10)
+		engine_force = Input.get_axis("ui_down","ui_up") * ENGINE_POWER
+	elif drift == true:
+		engine_force = 0
+		blwheel.wheel_friction_slip = 6
+		brwheel.wheel_friction_slip = 6
+		apply_torque_impulse(Vector3(0,20*Input.get_axis("ui_right","ui_left"),0))
+		
 		
 		
