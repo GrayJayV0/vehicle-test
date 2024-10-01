387948ab4f4695c364eadebb2101ab7268ddd734
GrayJayV0 <131674159+GrayJayV0@users.noreply.github.com>
2024-10-01 13:41:40 -0400

    base drift W.I.P
    The drift teleports when going into the opposite direction

--- block.gd
+++ block.gd
@@ -8,10 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if grounded.is_colliding():
-		#This vector is broken its missing a value needed to allow jumping
-		#Vectors are writen in (x,y,z) what value needs to be changed to allow jumping
-		apply_impulse(Vector3(0,0,0))
-		
-func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	state.transform = Transform3D(Vector3(0,0,1),Vector3(10,0,1),Vector3(0,0,1), position)
+	pass
--- car.gd
+++ car.gd
@@ -3,11 +3,14 @@ extends VehicleBody3D
 @onready var brwheel = $VehicleWheel3D
 @onready var flwheel = $VehicleWheel3D2
 @onready var frwheel = $VehicleWheel3D3
+@onready var camera = $Camera3D
 @export var MAX_STEER = 1
 @export var ENGINE_POWER = 2000
 var drift
 var new_rotation
 var old_position
+var old_velocity
+var axis
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,30 +19,48 @@ func _physics_process(delta: float) -> void:
 	#print(rotation_degrees,rotation)
 	if Input.is_action_pressed("drift") and Input.get_axis("ui_right","ui_left") != 0 and !drift:
 		drift = true
+		axis = Input.get_axis("ui_right","ui_left")
 		steering = Input.get_axis("ui_right","ui_left")
-		engine_force = 0
 		new_rotation = rotation_degrees
 		old_position = position
+		old_velocity = linear_velocity
 	if Input.is_action_just_released("drift"):
 		blwheel.wheel_friction_slip = 10.5
 		brwheel.wheel_friction_slip = 10.5
 		flwheel.wheel_friction_slip = 10.5
 		frwheel.wheel_friction_slip = 10.5
 		drift = false
-		
-		
+		angular_velocity.y == 0
+
 	if !drift:
 		steering = move_toward(steering, Input.get_axis("ui_right","ui_left") * MAX_STEER, delta * 10)
 		engine_force = Input.get_axis("ui_down","ui_up") * ENGINE_POWER
 	else:
-		engine_force = 0
+		linear_velocity = old_velocity
 		blwheel.wheel_friction_slip = 0
 		brwheel.wheel_friction_slip = 0	
-		apply_torque_impulse(Vector3(0,20*Input.get_axis("ui_right","ui_left"),0))
-		apply_central_impulse(Vector3(10,0,0))
+		apply_torque_impulse(Vector3(0,10*Input.get_axis("ui_right","ui_left"),0))
 		
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-	if drift:
-		var thing = clamp(rotation_degrees.y,new_rotation.y-45,new_rotation.y+45)
-		rotation_degrees.y = thing
-		
+	if drift and axis == 1:
+		if Input.get_axis("ui_right","ui_left") == 1:
+			var thing = clamp(rotation_degrees.y,0,new_rotation.y+75)
+			rotation_degrees.y = thing
+		if Input.get_axis("ui_right","ui_left") == 0:
+			var thing = clamp(rotation_degrees.y,0,new_rotation.y+55)
+			rotation_degrees.y = thing
+		elif Input.get_axis("ui_right","ui_left") == -1:
+			var thing = clamp(rotation_degrees.y,35,new_rotation.y+55)
+			rotation_degrees.y = thing
+	elif drift and axis == -1:
+		if Input.get_axis("ui_right","ui_left") == -1:
+			var thing = clamp(rotation_degrees.y,new_rotation.y-75,0)
+			rotation_degrees.y = thing
+			apply_central_impulse(Vector3(0,0,0))
+		if Input.get_axis("ui_right","ui_left") == 0:
+			var thing = clamp(rotation_degrees.y,new_rotation.y-55,0)
+			rotation_degrees.y = thing
+		elif Input.get_axis("ui_right","ui_left") == 1:
+			var thing = clamp(rotation_degrees.y,new_rotation.y-55,-35)
+			rotation_degrees.y = thing
+	print(rotation_degrees.y)
